578e8b64c55d76e8da575f354789ccbe357c5c5f
namaya <n3amaya@gmail.com>
2023-07-31 23:19:13 -0500

    feat(main.rs): add session cookies

--- inference-server/src/main.rs
+++ inference-server/src/main.rs
@@ -1,21 +1,75 @@
 #[macro_use] extern crate rocket;
 
+use std::fs;
+use std::path::Path;
+
+use rocket::http::{CookieJar, Cookie};
 use rocket::tokio::time::{Duration, interval};
 use rocket::response::stream::TextStream;
+use rocket::serde::{Deserialize, json::Json};
+use uuid::Uuid;
+
+const SESSION_COOKIE_NAME: &str = "chat-session-id";
+const SESSION_DATA_DIR: &str = "/tmp/chat-sessions";
 
 #[get("/")]
 fn index() -> &'static str { 
     "Hello, world!"
 }
 
-#[post("/generate")]
-fn generate() -> TextStream![&'static str] {
+#[derive(Deserialize)]
+#[serde(crate = "rocket::serde")]
+struct GenerateBody<'r> {
+    prompt: &'r str,
+}
+
+#[post("/generate", data="<body>")]
+fn generate(body: Json<GenerateBody<'_>>, jar: &CookieJar<'_>) -> TextStream![&'static str] {
+    let cookie = jar.get(SESSION_COOKIE_NAME);
+
+    let history = if let Some(cookie) = cookie {
+        let sess_id_str = cookie.value();
+        let path = Path::new(SESSION_DATA_DIR).join(sess_id_str);
+        match fs::read_to_string(path) {
+            Ok(history) => history,
+            Err(error) => {
+                panic!("should be 400 error");
+            }
+        }
+    } else {
+        let session_id = Uuid::new_v4();
+
+        jar.add(Cookie::new(SESSION_COOKIE_NAME, session_id.to_string()));
+
+        let path = Path::new(SESSION_DATA_DIR).join(session_id.to_string());
+
+        if path.exists() {
+            panic!("Writing new session history but session already exists...")
+        } else {
+            let history = "";
+
+            fs::write(path, history)
+                .expect("error while writing to fs.");
+
+            String::from(history)
+        }
+    };
+
+    println!("{history}");
+
     TextStream! {
         let mut interval = interval(Duration::from_secs(1));
+
         loop {
-            yield "hello";
+            yield "hello\n";
             interval.tick().await;
         }
+
+        // history.split_whitespace()
+        //     .map_async(|token| {
+        //         token
+        //     })
+
     }
 }
 
